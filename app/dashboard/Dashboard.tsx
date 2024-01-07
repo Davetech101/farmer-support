5ab3719e1d4a24adb2e6cb9b081fe1b62b723766
Ogaranya <enomahog@gmail.com>
2024-01-07 17:22:05 +0100

    dashboard

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+
+import { useState, useCallback } from "react";
+import { IoSearch } from "react-icons/io5";
+import { FaPlay } from "react-icons/fa";
+
 
 const Dashboard = () => {
   return (
     <section className=''>
         <h2 className=''>Welcome to farm Insight</h2>
+
+        <div className="">
+        <textarea name="" id="" className=""></textarea>
+
+        <button className=""><IoSearch /> <span className="">Deep Dive</span></button>
+        </div>
+
+        <p className="">Sample questions to ask <FaPlay /></p>
     </section>
   )
 }
