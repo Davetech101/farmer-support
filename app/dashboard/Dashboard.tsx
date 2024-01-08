03583a12f20b93c07828f4a4c22686285093850b
Ogaranya <enomahog@gmail.com>
2024-01-08 23:22:40 +0100

    dashboard

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -4,16 +4,20 @@ import { useState, useCallback } from "react";
 import { IoSearch } from "react-icons/io5";
 import { FaPlay } from "react-icons/fa";
 
+const sampleQuestions = [
+  "User-friendly AI interfaces for farmers", "What are effective pest control strategies for tomatoes", "Best methods for soil nutrient management and enrichment"
+]
+
 
 const Dashboard = () => {
   return (
-    <section className=''>
-        <h2 className=''>Welcome to farm Insight</h2>
+    <section className='flex justify-center items-center flex-col h-screen bg-primaryColor text-secondaryColor text-center p-8'>
+        <h2 className='text-5xl mb-10 text-[#718096]'>Welcome to farm Insight</h2>
 
-        <div className="">
-        <textarea name="" id="" className=""></textarea>
+        <div className="border h-32 border-borderColor rounded-lg w-full overflow-hidden relative">
+        <textarea name="" id="" className="w-full placeholder:text-2xl border-none text-primaryColor h-full p-4 outline-tetiaryColor" placeholder="Ask anything"></textarea>
 
-        <button className=""><IoSearch /> <span className="">Deep Dive</span></button>
+        <button className="absolute bottom-0 left-3 text-2xl flex items-center text-[#718096]"><IoSearch /> <span>Deep Dive</span></button>
         </div>
 
         <p className="">Sample questions to ask <FaPlay /></p>
