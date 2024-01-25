b4ef2d10f604a51ad5e5c4a481652085b2357767
Ogaranya <enomahog@gmail.com>
2024-01-25 20:39:13 +0100

    dashboard

--- app/_components/Menu.tsx
+++ app/_components/Menu.tsx
@@ -6,6 +6,9 @@ import { FaProductHunt } from "react-icons/fa";
 
 
 const Menu = () => {
+
+  const linkStyles = "text-2xl mb-10 flex items-center gap-[10px]"
+
   return (
     <div className='fixed top-0 left-0 w-full left-0'>
       <header className="bg-primaryColor p-10 text-secondaryColor flex w-full justify-between items-center">
@@ -19,10 +22,10 @@ const Menu = () => {
       <section className="w-screen absolute top-0 z-10">
         <div className=""></div>
 
-        <div className="w-9/12 bg-black1 p-10">
+        <div className="w-9/12 bg-tetiaryColor text-secondaryColor p-10">
           <div className="flex flex-col">
-            <Link href={"/dashboard"} className='text-2xl'>Dashboard</Link>
-            <Link href={"/listing"}>Product Listing</Link>
+            <Link href={"/dashboard"} className={linkStyles}><MdDashboard/> Dashboard</Link>
+            <Link href={"/listing"} className={linkStyles}><FaProductHunt/> Product Listing</Link>
           </div>
 
           <button className="">Log out</button>
